b7fd76e280bf5baf26a35d911015a88143d4f573
KhanhLy1395 <nguyenkhanhly1395@gmail.com>
2022-05-26 13:15:15 +0700

    lesson_02

--- appium-k9/src/lesson_03/lab_03_03.java
+++ appium-k9/src/lesson_03/lab_03_03.java
@@ -10,8 +10,16 @@ public class lab_03_03 {
         int[] intArr = {12, 34, 1, 16, 28};
 
         for (int i = 0; i < intArr.length; i++) {
-
+            for (int j = i + 1; j < intArr.length; j++) {
+                if (intArr[i] > intArr[j]) {
+                    int temp = intArr[i];
+                    intArr[i] = intArr[j];
+                    intArr[j] = temp;
+                }
+            }
         }
-
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.print(intArr[i] + " ");
+        }
     }
 }
---
+++ appium-k9/src/lesson_03/lab_03_04.java
@@ -0,0 +1,8 @@
+package lesson_03;
+
+public class lab_03_04 {
+
+    public static void main(String[] args) {
+
+    }
+}

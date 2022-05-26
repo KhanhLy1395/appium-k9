8475674ac32c5e4ca7f3a343e8620129a58349a7
KhanhLy1395 <nguyenkhanhly1395@gmail.com>
2022-05-26 13:19:28 +0700

    lesson_03

--- appium-k9/src/lesson_03/lab_03_04.java
+++ appium-k9/src/lesson_03/lab_03_04.java
@@ -3,6 +3,9 @@ package lesson_03;
 public class lab_03_04 {
 
     public static void main(String[] args) {
+    //    Array 01: {1, 12, 16, 28, 34}
+    //    Array 02: {1, 13, 16, 27, 99}
 
+    //    Expected output: {1, 1, 12, 13, 16, 16, 27, 28, 34, 99}
     }
 }

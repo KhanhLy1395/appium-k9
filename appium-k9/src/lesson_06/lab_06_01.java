d81d2d2c7768e605bb3a2ce1c3d73b5df006b960
KhanhLy1395 <nguyenkhanhly1395@gmail.com>
2022-06-07 16:42:54 +0700

    lesson_06

--- appium-k9/src/lesson_06/lab_06_01.java
+++ appium-k9/src/lesson_06/lab_06_01.java
@@ -3,6 +3,6 @@ package lesson_06;
 public class lab_06_01 {
 
     public static void main(String[] args) {
-
+        String inputTime =  "2hrs and 5 minutes";
     }
 }
---
+++ appium-k9/src/lesson_06/lab_06_02.java
@@ -0,0 +1,23 @@
+package lesson_06;
+
+import java.util.Scanner;
+
+public class lab_06_02 {
+
+    public static void main(String[] args) {
+        String myPassword = "password123";
+        int inputMaxTime = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.printf("Please input password");
+            String userInput = scanner.next();
+            if (!userInput.equals(myPassword)) {
+                System.out.println("Your password is wrong");
+                inputMaxTime++;
+            } else {
+                System.out.println("Log in successfully!!!");
+            }
+        } while (inputMaxTime < 3);
+
+    }
+}

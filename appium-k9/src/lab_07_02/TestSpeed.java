56d6b36af3b815e9adacc17e996277cff559b305
Nguyen Khanh Ly <nguyenkhanhly1395@gmail.com>
2022-06-20 21:55:26 +0700

    lab_07_02

--- appium-k9/src/lab_07_02/TestSpeed.java
+++ appium-k9/src/lab_07_02/TestSpeed.java
@@ -6,19 +6,27 @@ public class TestSpeed {
         Animal tiger = new Tiger();
         Animal horse = new Horse();
 
-       int dogSpeed = dog.getSpeed();
-       int horseSpeed = horse.getSpeed();
-       int tigerSpeed = tiger.getSpeed();
+        int dogSpeed = dog.getSpeed();
+        int horseSpeed = horse.getSpeed();
+        int tigerSpeed = tiger.getSpeed();
 
         System.out.println("Dog: " + dogSpeed);
         System.out.println("Tiger: " + tigerSpeed);
         System.out.println("Horse: " + horseSpeed);
+
+        System.out.println(dog.getClass().getSimpleName());
         if (dogSpeed > horseSpeed && dogSpeed > tigerSpeed)
-           System.out.println("Dog is winner with speed: " + dogSpeed);
-       else if (horseSpeed > dogSpeed && horseSpeed >tigerSpeed)
-           System.out.println("Horse is winner with speed: " + horseSpeed);
-       else if (tigerSpeed > horseSpeed && tigerSpeed > dogSpeed)
-           System.out.println("Tiger is winner with speed: " + tigerSpeed);
+            System.out.println("Dog is winner with speed: " + dogSpeed);
+        else if (horseSpeed > dogSpeed && horseSpeed >tigerSpeed)
+            System.out.println("Horse is winner with speed: " + horseSpeed);
+        else if (tigerSpeed > horseSpeed && tigerSpeed > dogSpeed)
+            System.out.println("Tiger is winner with speed: " + tigerSpeed);
+        else {
+            System.out.println("There are at least 2 animal has the same speed");
+        }
+
+
+
 
     }
 
